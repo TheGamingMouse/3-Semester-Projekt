7c4a0d4e96eb28e3057c0491b2fc094cb7da62d0
hopefulbastard <miloskousgaard@gmail.com>
2023-12-15 10:54:59 +0100

    Cors

--- 3_Semester_Projekt_API/Program.cs
+++ 3_Semester_Projekt_API/Program.cs
@@ -11,6 +11,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(option =>
+            option.AddPolicy("Allow All",
+            builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader())
+            );
+
 var optionsBuilder = new DbContextOptionsBuilder<SikkerhedsLogDBContext>();
 optionsBuilder.UseSqlServer("Data Source = mssql5.unoeuro.com; Initial Catalog = bbksolutions_dk_db_databasen; User ID = bbksolutions_dk; Password=cmfbeAtrkR5zBaF426x3;Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent = ReadWrite; MultiSubnetFailover=False");
 SikkerhedsLogDBContext context = new SikkerhedsLogDBContext(optionsBuilder.Options);
@@ -26,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
+app.UseCors("Allow All");
 
 app.MapControllers();
 
